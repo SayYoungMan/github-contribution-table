3b4eb19465365833ced236596527331948914720
SayYoungMan <nickte89@gmail.com>
2023-07-08 17:23:08 +0100

    footer mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outer-most-border">
+      <div className="outer-table-container">
+        <div className="inner-table-container"></div>
+        <div className="footer-container">
+          <div className="footer-caption-container">
+            <a
+              href="https://docs.github.com/en/account-and-profile/setting-up-and-managing-your-github-profile/managing-contribution-settings-on-your-profile/why-are-my-contributions-not-showing-up-on-my-profile"
+              className="footer-caption"
+            >
+              Learn how we count contributions
+            </a>
+          </div>
+          <div className="footer-legend-container">
+            Less
+            <svg>
+              <rect className="level-0-grid" />
+            </svg>
+            <svg>
+              <rect className="level-1-grid" />
+            </svg>
+            <svg>
+              <rect className="level-2-grid" />
+            </svg>
+            <svg>
+              <rect className="level-3-grid" />
+            </svg>
+            <svg>
+              <rect className="level-4-grid" />
+            </svg>
+            More
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
